538f36b8c15219120d10cc7355764912c7478fb5
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-08-29 12:39:01 +0500

    Done with to show the all the launches and details of the launches

--- spacex/src/components/LaunchDetails/LaunchDetails.tsx
+++ spacex/src/components/LaunchDetails/LaunchDetails.tsx
@@ -15,6 +15,19 @@ const LaunchDetails: React.FC<Props>=({data})=>{
             <div className="LaunchDetailsStatus">
                 <span>Flight {data.launch.flight_number}</span>
             </div>
+            <h1>
+                {data.launch.mission_name} - {data.launch.rocket?.rocket_name}
+            </h1>
+            <p>Launch from {data.launch.launch_site?.site_name} in {data.launch.launch_year}</p>
+            <p>{data.launch.details}</p>
+            {!!data.launch.links && !!data.launch.links.flickr_images && (
+                <div>
+                    {data.launch.links.flickr_images.map(image=>{
+                        image? <img src={image} alt="" />:null
+                    })}
+                </div>
+            )}
+            <img src="" alt="" />
         </div>
     )
 }
