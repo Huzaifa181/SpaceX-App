3d09b81915457467aad55e4a705c2e20e121ddd2
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-08-29 12:06:18 +0500

    Done with frontend to show list of launches

--- spacex/src/components/Launch/Launch.tsx
+++ spacex/src/components/Launch/Launch.tsx
@@ -1,1 +1,24 @@
-export {}
+import React from 'react'
+import {LaunchListQuery} from '../../generated/graphql'
+import './style.css'
+
+interface Props{
+    data: LaunchListQuery;
+}
+
+const Launch: React.FC<Props> = {{data}} =>{
+    <div className="Launches">
+        <h3>All SpaceX Launches</h3>
+        <ol className="LaunchesOL">
+            {!!data.launches && data.launches.map((launch, i)=>{
+                return !!launch && (
+                    <li key={i} className="LaunchesItem">
+                        {launch.mission_name} - {launch.launch_year} ({launch.launch_success})
+                    </li>
+                )
+            })}
+        </ol>
+    </div>
+}
+
+export default Launch
--- spacex/src/components/Launch/index.tsx
+++ spacex/src/components/Launch/index.tsx
@@ -1,1 +1,18 @@
-export {}
+import React from 'react'
+import {useLaunchListQuery} from '../../generated/graphql'
+import Launch from './Launch'
+
+const LaunchContainer=()=>{
+    const {data, error, loading} = useLaunchListQuery();
+
+    if(loading){
+        return <div>Data is Loading</div>
+    }
+    if(error || !data){
+        return <div>There was an error</div>
+    }
+
+    return <Launch data={data}></Launch>
+}
+
+export default LaunchContainer;
