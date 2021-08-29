1ff91289ce9703fa978f3361d8a65c475c3e7a1d
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-08-29 11:22:03 +0500

    Solve Error

--- spacex/src/index.js
+++ spacex/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {gql, ApolloProvider, InMemoryCache, ApolloClient} from 'apollo/client'
+import {gql, ApolloProvider, InMemoryCache, ApolloClient} from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://spacexdata.herokuapp.com/graphql',
